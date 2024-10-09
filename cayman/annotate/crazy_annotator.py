8f601d4d0e81d780dd4861509967f0fdda4926ac
Nicolai Karcher <karchernic@gmail.com>
2024-10-09 11:09:50 +0200

    Fixing pandas FutureWarning #1

--- cayman/annotate/crazy_annotator.py
+++ cayman/annotate/crazy_annotator.py
@@ -105,7 +105,7 @@ class CazyAnnotator:
         import pandas as pd
         median_cutoffs = pd.read_csv(precomputed_hmm_cutoffs)
         median_cutoffs['familyType'] = [sub("\d", "", x.replace("_", "")) for x in median_cutoffs['family']]
-        median_cutoffs = median_cutoffs.groupby('familyType').median()['cutoff']
+        median_cutoffs = median_cutoffs.groupby('familyType').median(numeric_only=True)['cutoff']
         cutoffs_all = pd.read_csv(precomputed_hmm_cutoffs)
         family_fold_results_filtered = []
         for index, family_fold_results in enumerate(self.annotations_by_family_and_fold):
